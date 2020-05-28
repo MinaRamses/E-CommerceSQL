3189e82976e7425bad3581a794c9cdf47d617ef2
MinaRamses <52183023+MinaRamses@users.noreply.github.com>
2020-05-28 21:32:37 +0200

    Add files via upload

---
+++ M3/activateven.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="activateven.aspx.cs" Inherits="M3.activateven" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            position: absolute;
+            top: 46px;
+            left: 12px;
+            z-index: 1;
+        }
+        .auto-style2 {
+            position: absolute;
+            top: 90px;
+            left: 14px;
+            z-index: 1;
+        }
+        .auto-style3 {
+            position: absolute;
+            top: 90px;
+            left: 137px;
+            z-index: 1;
+            width: 63px;
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter Vendor Username to get activated:</div>
+        <asp:TextBox ID="TextBox1" runat="server" CssClass="auto-style1"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" CssClass="auto-style2" Text="Activate" OnClick="activate_ven" />
+        <asp:Button ID="Button2" runat="server" CssClass="auto-style3" Text="Back" OnClick="Button2_Click" />
+    </form>
+</body>
+</html>
---
+++ M3/activateven.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace M3
+{
+    public partial class activateven : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminhomepage.aspx");
+        }
+
+        protected void activate_ven(object sender, EventArgs e)
+        {
+            //Get the information of the connection to the database
+            string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+
+            //create a new connection
+            SqlConnection conn = new SqlConnection(connStr);
+
+            /*create a new SQL command which takes as parameters the name of the stored procedure and
+             the SQLconnection name*/
+            SqlCommand cmd = new SqlCommand("activateVendors", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            //To read the input from the user
+            string username = (String)(Session["aname"]);
+            string vendername = TextBox1.Text;
+           
+
+
+            //pass parameters to the stored procedure
+            cmd.Parameters.Add(new SqlParameter("@admin_username", username));
+            cmd.Parameters.Add(new SqlParameter("@vendor_username", vendername));
+           
+
+            //Executing the SQLCommand
+
+            if (username.ToString().Equals(""))
+            {
+                Response.Write("Enter all the empty fields");
+            }
+            else
+            {
+                conn.Open();
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    Session["aname"] = username;
+                    Response.Write("Vendor has been activated");
+                }
+                catch (Exception)
+                {
+                    Response.Write("Vendor username already exsist");
+                }
+
+            }
+        }
+    }
+}
